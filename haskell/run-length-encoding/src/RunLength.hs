1c457047b73ecd414da4686a36cb651631874408
gil lawson <gewl@users.noreply.github.com>
2017-03-20 23:37:18 -0400

    decode complete

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -5,10 +5,24 @@ import Text.Regex.Posix
 import Data.List (intercalate)
 
 decode :: String -> String
-decode cs = cc ( getAllTextMatches $ cs =~ "[0-9]*[[:alpha:]]" :: [String] )
+decode cs = cc . getAllTextMatches $ cs =~ "[0-9]*[[:alpha:]]" 
 
 encode :: String -> String
 encode = error "You need to implement this function."
 
 cc :: [ String ] -> String
-cc s = intercalate "a " s
+cc s = intercalate "" $ ep s
+
+ep :: [ String ] -> [ String ]
+ep s = map f s
+
+f :: String -> String
+f cp = case length cp of
+		1 -> cp
+		_ -> duplicate (read $ init cp :: Int) (cts $ last cp)
+
+duplicate :: Int -> String -> String
+duplicate n sl = concat $ replicate n sl
+
+cts :: Char -> String
+cts = (:[])
