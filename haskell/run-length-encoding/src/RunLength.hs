659e57efd2bcfde012b6ee2c9131ee62c766b641
gil lawson <gewl@users.noreply.github.com>
2017-03-21 00:46:55 -0400

    Encode/decode complete. Haskell doesn't have backreferencing.

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,20 +1,16 @@
 module RunLength (decode, encode) where
 
-import Text.Regex ( matchRegex, mkRegex )
-import Text.Regex.Posix 
 import Data.List (intercalate)
+import Text.Regex.PCRE
 
 decode :: String -> String
-decode cs = cc . getAllTextMatches $ cs =~ "[0-9]*[[:alpha:]]" 
+decode cs = cc f ( getAllTextMatches $ cs =~ "[0-9]*[[:alpha:]]|[[:blank:]]" ) 
 
-encode :: String -> String
-encode = error "You need to implement this function."
+cc :: (String -> String) -> [ String ] -> String
+cc x s = intercalate "" $ ep x s
 
-cc :: [ String ] -> String
-cc s = intercalate "" $ ep s
-
-ep :: [ String ] -> [ String ]
-ep s = map f s
+ep :: (String -> String) -> [ String ] -> [ String ]
+ep x s = map x s
 
 f :: String -> String
 f cp = case length cp of
@@ -26,3 +22,13 @@ duplicate n sl = concat $ replicate n sl
 
 cts :: Char -> String
 cts = (:[])
+
+-- Oh my god, Haskell doesn't have backreferencing.
+encode :: String -> String
+encode fs = cc g ( getAllTextMatches $ fs =~ "[[:blank:]]|A*|B*|C*|D*|E*|F*|G*|H*|I*|J*|K*|L*|M*|N*|O*|P*|Q*|R*|S*|T*|U*|V*|W*|X*|Y*|Z*|a*|b*|c*|d*|e*|f*|g*|h*|i*|j*|k*|l*|m*|n*|o*|p*|q*|r*|s*|t*|u*|v*|w*|x*|y*|z*" )
+
+g :: String -> String
+g cp = case length cp of
+		0 -> ""
+		1 -> cp
+		len -> ( show len ) ++ (cts $ head cp)
